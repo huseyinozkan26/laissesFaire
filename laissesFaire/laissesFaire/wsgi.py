abcca3998b2ee0fac24b48a69afdad8e6b8bd3ae
huseyinozkan26 <ozkanhuseyin@yandex.com.tr>
2023-11-20 22:58:00 +0300

    back to wsgi.py

--- laissesFaire/laissesFaire/wsgi.py
+++ laissesFaire/laissesFaire/wsgi.py
@@ -1,11 +1,16 @@
+"""
+WSGI config for laissesFaire project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'laissesFaire.settings')  # Projeye özgü settings modülüne ayarlayın
-
-# Sanal ortamın yolunu ekleyin (eğer kullanılıyorsa)
-activate_this = '/var/www/django/laissesFaire/lfEnv/bin/activate'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'laissesFaire.settings')
 
 application = get_wsgi_application()
