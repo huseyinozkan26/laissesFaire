1a3212e6b9aa211be5de96763f27fd2189923dcc
huseyinozkan26 <ozkanhuseyin@yandex.com.tr>
2023-11-20 22:49:10 +0300

    host settings

--- laissesFaire/laissesFaire/settings.py
+++ laissesFaire/laissesFaire/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-dk(u4-cs46le%0yaj%q%#pma1f9u@+%0%viohou)3g@)*k=(vx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['lf8056.com.tr', '45.139.199.131', 'localhost', '127.0.0.1']
 
 # Application definition
 
