32a4de30f66142b29983d0cbf56e36a51132e081
huseyinozkan26 <ozkanhuseyin@yandex.com.tr>
2023-11-20 22:11:52 +0300

    wsqi ayarları 2

--- laissesFaire/laissesFaire/wsgi.py
+++ laissesFaire/laissesFaire/wsgi.py
@@ -4,7 +4,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'laissesFaire.settings')  # Projeye özgü settings modülüne ayarlayın
 
 # Sanal ortamın yolunu ekleyin (eğer kullanılıyorsa)
-activate_this = '/var/www/django/laissesFaire/lfEnv/bin/activate_this.py'
+activate_this = '/var/www/django/laissesFaire/lfEnv/bin/activate'
 with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
 
