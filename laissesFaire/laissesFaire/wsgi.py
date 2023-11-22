0d3f7bcf2b5976855cc71d370709c7aa9e03f02a
huseyinozkan26 <ozkanhuseyin@yandex.com.tr>
2023-11-22 22:12:25 +0300

    wsgi set 2

--- laissesFaire/laissesFaire/wsgi.py
+++ laissesFaire/laissesFaire/wsgi.py
@@ -11,7 +11,7 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-path = '/var/www/django/laissesFaire'  # Django proje dizininin yolu
+path = '/var/www/django/laissesFaire/laissesFaire'  # Django proje dizininin yolu
 if path not in sys.path:
     sys.path.append(path)
 
