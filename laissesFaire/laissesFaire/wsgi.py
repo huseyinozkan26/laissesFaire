c71efd059f314240c13ebc0daccc0214cf9de5c2
huseyinozkan26 <ozkanhuseyin@yandex.com.tr>
2023-11-22 22:38:45 +0300

    wsgi set 6

--- laissesFaire/laissesFaire/wsgi.py
+++ laissesFaire/laissesFaire/wsgi.py
@@ -8,11 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-import django
-from django.core.handlers.wsgi import WSGIHandler
 
+from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eisentask.settings.production")
-django.setup(set_prefix=False)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'laissesFaire.settings')
 
-application = WSGIHandler()
+application = get_wsgi_application()
